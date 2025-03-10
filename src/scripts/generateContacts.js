412e4f697bfaaf5b2cfab37249322ae428d274d5
Lana150 <astra.altair150@gmail.com>
2025-03-10 23:25:02 +0100

    added changes

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,14 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+      const allContacts = await readContacts();
+  for (let index = 0; index < number; index++) {
+    allContacts.push(createFakeContact());
+  }
+
+  await writeContacts(allContacts);
+ };
 
 generateContacts(5);
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,11 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+     try {
+    await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts), 'utf8');
+  } catch (err) {
+    throw `File writing error:', ${err}`;
+  }
+};
+
